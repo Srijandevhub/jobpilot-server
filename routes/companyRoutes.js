cd0db2ed751647ff42ac5ddb2639e7d91bf9038a
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-04-16 14:46:03 +0530

    Update companyRoutes.js

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { verifyAuthentication, verifySuperAdmin, verifyCompanyAdmin } = require('../middlewares/auth');
-const { addCompany, getCompanies, getMyCompany, updateMyCompany } = require('../controllers/companyControllers');
+const { addCompany, getCompanies, getMyCompany, updateMyCompany, updateMyCompanyCover, updateMyCompanyLogo } = require('../controllers/companyControllers');
+const upload = require('../middlewares/upload');
 const router = express.Router();
 
 router.post("/addcompany", verifyAuthentication, verifySuperAdmin, addCompany);
 router.get("/allcompanies", verifyAuthentication, verifySuperAdmin, getCompanies);
 router.get("/mycompany", verifyAuthentication, verifyCompanyAdmin, getMyCompany);
 router.put("/updatemycompany", verifyAuthentication, verifyCompanyAdmin, updateMyCompany);
+router.post("/updatemycompanycover", verifyAuthentication, verifyCompanyAdmin, upload.single("imgfile"), updateMyCompanyCover);
+router.post("/updatemycompanylogo", verifyAuthentication, verifyCompanyAdmin, upload.single("imgfile"), updateMyCompanyLogo);
 
 module.exports = router;
