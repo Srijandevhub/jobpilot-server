3d88952f805fd9b7be45fbbde24993ff95e8d170
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-04-16 16:40:29 +0530

    rc

--- models/resumeModel.js
+++ models/resumeModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const resumeSchema = new mongoose.Schema({
     url: { type: String },
-    userid: { tpye: mongoose.Schema.Types.ObjectId, ref: 'users' }
+    userid: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    publicid: { type: String }
 }, { timestamps: true });
 
 const Resume = mongoose.model("resumes", resumeSchema);
