4d991cc5e5b806abf327bf7f01684b5165562758
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-04-30 15:51:31 +0530

    Update categoryControllers.js

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,21 +1,39 @@
 const Category = require('../models/categoryModel');
+const cloudinary = require('../config/cloudinaryConfig');
 
 const addCategory = async (req, res) => {
     try {
+        const user = req.user;
+        if (user.role === 'applicant') {
+            return res.status(400).json({ message: "Access denied: add category not allowed" });
+        }
         const { title } = req.body;
-        const icon = req.file.path || '';
+        const icon = req.file;
+
         if (!title.trim()) {
             return res.status(400).json({ message: "Title is required" });
         }
-        if (!icon.trim()) {
+        if (!icon) {
             return res.status(400).json({ message: "Icon is required" });
         }
-        const newCategoty = new Category({
-            icon,
+        const newCategory = new Category({
             title
         });
-        await newCategoty.save();
-        res.status(200).json({ message: "Category added", category: newCategoty });
+        await newCategory.save();
+        const publicId = `jobpilot-category-${newCategory._id}`;
+        const stream = cloudinary.uploader.upload_stream({
+            folder: "jobpilot_category",
+            public_id: publicId,
+            overwrite: true,
+            resource_type: 'image'
+        }, async (error, result) => {
+            if (error) {
+                return res.status(500).json({ message: "Upload failed: Cloudinary error", error: error.message });
+            }
+            await Category.findByIdAndUpdate(newCategory._id, { icon: result.secure_url });
+        });
+        stream.end(icon.buffer);
+        res.status(200).json({ message: "Category added" });
     } catch (error) {
         res.status(500).json({ message: "Internal server error", error: error.message });
     }
@@ -23,7 +41,11 @@ const addCategory = async (req, res) => {
 
 const getCategories = async (req, res) => {
     try {
-        const categories = await Category.find();
+        const user = req.user;
+        if (user.role === 'applicant') {
+            return res.status(400).json({ message: "Access denied: get categories not allowed" });
+        }
+        const categories = await Category.find().sort({ createdAt: -1 });
         res.status(200).json({ message: "Categories fetched", categories: categories });
     } catch (error) {
         res.status(500).json({ message: "Internal server error", error: error.message });
