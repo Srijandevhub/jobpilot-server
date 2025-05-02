f1b9c0ee99b90569e298d33cf58fca8dea51bbd4
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-05-02 12:46:21 +0530

    Update publicRoutes.js

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getCategoriesPublic, getJobsPublic, getJobrolesPublic, getJobPublic } = require('../controllers/publicControllers');
+const { getCategoriesPublic, getJobsPublic, getJobrolesPublic, getJobPublic, getCompaniesPublic, getCompanyPublic } = require('../controllers/publicControllers');
 const router = express.Router();
 
 router.get("/categories", getCategoriesPublic);
 router.get("/jobs", getJobsPublic);
 router.get("/jobroles", getJobrolesPublic);
 router.get("/job/:id", getJobPublic);
+router.get("/companies", getCompaniesPublic);
+router.get("/company/:id", getCompanyPublic);
 
 module.exports = router;
