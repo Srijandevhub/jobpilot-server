0cbe01f445ab08717bc87ecc99ebd7cfa167c354
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-05-03 16:51:39 +0530

    rc

---
+++ models/applicationModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const applicationSchema = new mongoose.Schema({
+    jobid: { type: mongoose.Schema.Types.ObjectId, ref: 'jobs' },
+    userid: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    resumelink: { type: String, required: true },
+    coverletter: { type: String, required: true },
+    status: { type: String, enum: ['applied', 'under_review', 'rejected', 'accepted'], default: 'applied' }
+}, { timestamps: true });
+
+const Application = mongoose.model("applications", applicationSchema);
+
+module.exports = Application;
