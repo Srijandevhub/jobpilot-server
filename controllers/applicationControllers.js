cad48db254fceb41ffbd59e1c58263500c90381e
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-05-05 11:39:33 +0530

    Update applicationControllers.js

--- controllers/applicationControllers.js
+++ controllers/applicationControllers.js
@@ -42,4 +42,24 @@ const updateApplication = async (req, res) => {
     }
 }
 
-module.exports = { addApplication, updateApplication };
+const getApplications = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const applications = await Application.find({ jobid: id }).sort({ createdAt: -1 });
+        const userids = applications.map(item => item.userid);
+        const users = await User.find({ _id: { $in: userids } });
+        const allApplication = applications.map((item) => {
+            const userid = item.userid;
+            const user = users.find(item => item._id.toString() === userid.toString());
+            return {
+                _id: item._id,
+                applicant: user.fullname,
+            }
+        })
+        res.status(200).json({ message: "Fetched applications", applications: allApplication });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+
+module.exports = { addApplication, updateApplication, getApplications };
