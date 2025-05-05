462f50f918507dbbb45ccf95274bc4cbb2248e2b
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-05-05 11:39:46 +0530

    Update applicationRoutes.js

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { verifyAuthentication } = require('../middlewares/auth');
-const { addApplication, updateApplication } = require('../controllers/applicationControllers');
+const { addApplication, updateApplication, getApplications } = require('../controllers/applicationControllers');
 const router = express.Router();
 
 router.post("/", verifyAuthentication, addApplication);
 router.put("/", verifyAuthentication, updateApplication);
+router.get("/:id", verifyAuthentication, getApplications)
 
 module.exports = router;
